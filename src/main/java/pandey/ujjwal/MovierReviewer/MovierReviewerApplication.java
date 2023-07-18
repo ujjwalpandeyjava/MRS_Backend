709caf23915349b186b5a15bec81481ec148553b
Ujjwal_Pandey Office <ujjwalpandey.aps@gmail.com>
2023-07-18 11:50:44 +0530

    Added Master controller and fixed APIs

--- src/main/java/pandey/ujjwal/MovierReviewer/MovierReviewerApplication.java
+++ src/main/java/pandey/ujjwal/MovierReviewer/MovierReviewerApplication.java
@@ -7,8 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 @RestController
 public class MovierReviewerApplication {
-
-	// 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
 	public static void main(String[] args) {
 		SpringApplication.run(MovierReviewerApplication.class, args);
 	}
--- src/main/java/pandey/ujjwal/MovierReviewer/pojo/Movie.java
+++ src/main/java/pandey/ujjwal/MovierReviewer/pojo/Movie.java
@@ -7,15 +7,7 @@ import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.mongodb.core.mapping.DocumentReference;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-import lombok.ToString;
-
 @Document(collection = "movies")
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
 public class Movie {
 	@Id
 	private ObjectId id;
@@ -26,9 +18,99 @@ public class Movie {
 	private List<String> genres;
 	private String poster;
 	private List<String> backdrops;
-	// Store on the Id: manual Referal realtionShip
+	// Manual Referral RealtionShip
 	@DocumentReference
 	private List<Review> reviewIds;
+
+	public Movie() {
+		super();
+	}
+
+	public Movie(ObjectId id, String imdbId, String title, String releaseDate, String trailerLink, List<String> genres,
+			String poster, List<String> backdrops, List<Review> reviewIds) {
+		super();
+		this.id = id;
+		this.imdbId = imdbId;
+		this.title = title;
+		this.releaseDate = releaseDate;
+		this.trailerLink = trailerLink;
+		this.genres = genres;
+		this.poster = poster;
+		this.backdrops = backdrops;
+		this.reviewIds = reviewIds;
+	}
+
+	public ObjectId getId() {
+		return id;
+	}
+
+	public void setId(ObjectId id) {
+		this.id = id;
+	}
+
+	public String getImdbId() {
+		return imdbId;
+	}
+
+	public void setImdbId(String imdbId) {
+		this.imdbId = imdbId;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getReleaseDate() {
+		return releaseDate;
+	}
+
+	public void setReleaseDate(String releaseDate) {
+		this.releaseDate = releaseDate;
+	}
+
+	public String getTrailerLink() {
+		return trailerLink;
+	}
+
+	public void setTrailerLink(String trailerLink) {
+		this.trailerLink = trailerLink;
+	}
+
+	public List<String> getGenres() {
+		return genres;
+	}
+
+	public void setGenres(List<String> genres) {
+		this.genres = genres;
+	}
+
+	public String getPoster() {
+		return poster;
+	}
+
+	public void setPoster(String poster) {
+		this.poster = poster;
+	}
+
+	public List<String> getBackdrops() {
+		return backdrops;
+	}
+
+	public void setBackdrops(List<String> backdrops) {
+		this.backdrops = backdrops;
+	}
+
+	public List<Review> getReviewIds() {
+		return reviewIds;
+	}
+
+	public void setReviewIds(List<Review> reviewIds) {
+		this.reviewIds = reviewIds;
+	}
 
 	@Override
 	public String toString() {
